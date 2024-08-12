ca7cf8cb839c1885cad702f6b6bca0deaa02ecdd
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-08-12 17:11:09 -0300

    Arreglado bug: un aventurero que murio podia tener una recompensa ademas de la recompensa con 0 oro

--- src/parcial.pl
+++ src/parcial.pl
@@ -9,41 +9,41 @@ poseeCaracteristica(kelsier, noble).
 poseeCaracteristica(thorfinn, agresivo).
 poseeCaracteristica(thorfinn, fuerte).
 
-% personaje(Nombre, Clase, Oro)
-personaje(rin, mago, 20).
-personaje(atalanta, mago, 30).
-personaje(kelsier, luchador, 50).
-personaje(thorfinn, barbaro, 15).
+% aventurero(Nombre, Clase, Oro)
+aventurero(rin, mago, 20).
+aventurero(atalanta, mago, 30).
+aventurero(kelsier, luchador, 50).
+aventurero(thorfinn, barbaro, 15).
 
 tiene(kelsier, permisoReal).
 
-camaradas(UnPersonaje, OtroPersonaje):-
-    personaje(UnPersonaje, Clase, _),
-    personaje(OtroPersonaje, Clase, _),
-    UnPersonaje \= OtroPersonaje.
+camaradas(UnAventurero, OtroAventurero):-
+    aventurero(UnAventurero, Clase, _),
+    aventurero(OtroAventurero, Clase, _),
+    UnAventurero \= OtroAventurero.
 
 caracteristicaPopular(Caracteristica, Clase):-
-    personaje(_, Clase, _),
+    aventurero(_, Clase, _),
     poseeCaracteristica(_, Caracteristica),
     forall(
-        personaje(Personaje, Clase, _),
-        poseeCaracteristica(Personaje, Caracteristica)
+        aventurero(Aventurero, Clase, _),
+        poseeCaracteristica(Aventurero, Caracteristica)
     ).
 
-puedeDisponderDePermisoReal(Personaje):-
-    tiene(Personaje, permisoReal).
-puedeDisponderDePermisoReal(Personaje):-
-    tieneFondosParaComprar(Personaje, permisoReal).
+puedeDisponderDePermisoReal(Aventurero):-
+    tiene(Aventurero, permisoReal).
+puedeDisponderDePermisoReal(Aventurero):-
+    tieneFondosParaComprar(Aventurero, permisoReal).
 
 precio(permisoReal, 50).
 precio(pociones, 20).
 
-tieneFondosParaComprar(Personaje, Objeto):-
+tieneFondosParaComprar(Aventurero, Objeto):-
     precio(Objeto, Precio),
-    personaje(Personaje, _, Oro),
+    aventurero(Aventurero, _, Oro),
     Oro >= Precio.
 
-:- begin_tests_con(parte1, [personaje(guybrush, pirata, 50)]).
+:- begin_tests_con(parte1, [aventurero(guybrush, pirata, 50)]).
 
 test("Dos aventureros son camaradas si son de la misma clase"):-
     camaradas(rin, atalanta).
@@ -69,39 +69,39 @@ test("Un aventurero puede disponer de un permiso real si puede comprarlo (valen 
 
 % Parte 2
 
-% conoceHechizo(Personaje, Elemento, Potencia)
+% conoceHechizo(Aventurero, Elemento, Potencia)
 conoceHechizo(rin, fuego, 20).
 conoceHechizo(rin, frio, 20).
 conoceHechizo(atalanta, fuego, 30).
 
-puedeRealizar(Personaje, ayudarACruzarLaCalle):-
-    personaje(Personaje, _, _).
-puedeRealizar(Personaje, ayudarACortarLenia):-
-    poseeCaracteristica(Personaje, fuerte).
-puedeRealizar(Personaje, escoltaRealDe(diplomatico)):-
-    puedeDisponderDePermisoReal(Personaje).
-puedeRealizar(Personaje, escoltaRealDe(princesa)):-
-    tiene(Personaje, permisoReal),
-    poseeCaracteristica(Personaje, noble).
-puedeRealizar(Personaje, pesadillaDeLaCueva):-
-    personaje(Personaje, barbaro, _),
-    poseeCaracteristica(Personaje, agresivo).
-puedeRealizar(Personaje, pesadillaDeLaCueva):-
-    conoceHechizoTanPoderosoComo(Personaje, fuego, 30).
-puedeRealizar(Personaje, rosasHeladas(Lugar)):-
+puedeRealizar(Aventurero, ayudarACruzarLaCalle):-
+    aventurero(Aventurero, _, _).
+puedeRealizar(Aventurero, ayudarACortarLenia):-
+    poseeCaracteristica(Aventurero, fuerte).
+puedeRealizar(Aventurero, escoltaRealDe(diplomatico)):-
+    puedeDisponderDePermisoReal(Aventurero).
+puedeRealizar(Aventurero, escoltaRealDe(princesa)):-
+    tiene(Aventurero, permisoReal),
+    poseeCaracteristica(Aventurero, noble).
+puedeRealizar(Aventurero, pesadillaDeLaCueva):-
+    aventurero(Aventurero, barbaro, _),
+    poseeCaracteristica(Aventurero, agresivo).
+puedeRealizar(Aventurero, pesadillaDeLaCueva):-
+    conoceHechizoTanPoderosoComo(Aventurero, fuego, 30).
+puedeRealizar(Aventurero, rosasHeladas(Lugar)):-
     categoria(rosasHeladas(Lugar), _),
-    tieneFondosParaComprar(Personaje, pociones),
-    conoceHechizoTanPoderosoComo(Personaje, frio, 20).
+    tieneFondosParaComprar(Aventurero, pociones),
+    conoceHechizoTanPoderosoComo(Aventurero, frio, 20).
 
-conoceHechizoTanPoderosoComo(Personaje, Elemento, PotenciaMinima):-
-    conoceHechizo(Personaje, Elemento, Potencia),
+conoceHechizoTanPoderosoComo(Aventurero, Elemento, PotenciaMinima):-
+    conoceHechizo(Aventurero, Elemento, Potencia),
     Potencia >= PotenciaMinima.
 
 facil(Mision):-
     puedeRealizar(_, Mision),
     forall(
-        personaje(Personaje, _, _),
-        puedeRealizar(Personaje, Mision)
+        aventurero(Aventurero, _, _),
+        puedeRealizar(Aventurero, Mision)
     ).
 
 categoria(ayudarACruzarLaCalle, deBarrio).
@@ -120,21 +120,21 @@ intento(thorfinn, pesadillaDeLaCueva).
 intento(atalanta, ayudarACruzarLaCalle).
 intento(atalanta, ayudarACortarLenia).
 
-resultado(Personaje, Mision, exitoso):-
-    intento(Personaje, Mision),
-    puedeRealizar(Personaje, Mision).
-resultado(Personaje, Mision, Resultado):-
-    intento(Personaje, Mision),
-    not(puedeRealizar(Personaje, Mision)),
+resultado(Aventurero, Mision, exitoso):-
+    intento(Aventurero, Mision),
+    puedeRealizar(Aventurero, Mision).
+resultado(Aventurero, Mision, Resultado):-
+    intento(Aventurero, Mision),
+    not(puedeRealizar(Aventurero, Mision)),
     categoria(Mision, Categoria),
     resultadoNegativo(Categoria, Resultado).
-% resultado(Personaje, Mision, fatal):-
-%     intento(Personaje, Mision),
-%     not(puedeRealizar(Personaje, Mision)),
+% resultado(Aventurero, Mision, fatal):-
+%     intento(Aventurero, Mision),
+%     not(puedeRealizar(Aventurero, Mision)),
 %     categoria(Mision, heroica).
-% resultado(Personaje, Mision, fallido):-
-%     intento(Personaje, Mision),
-%     not(puedeRealizar(Personaje, Mision)),
+% resultado(Aventurero, Mision, fallido):-
+%     intento(Aventurero, Mision),
+%     not(puedeRealizar(Aventurero, Mision)),
 %     categoria(Mision, Categoria),
 %     Categoria \= heroica.
 
@@ -142,19 +142,20 @@ resultadoNegativo(heroica, fatal).
 resultadoNegativo(aspirante, fallido).
 resultadoNegativo(deBarrio, fallido).
 
-afortunado(Personaje):-
-    personaje(Personaje, _, _),
-    forall(intento(Personaje, Mision),
-            resultado(Personaje, Mision, exitoso)).
+afortunado(Aventurero):-
+    aventurero(Aventurero, _, _),
+    forall(intento(Aventurero, Mision),
+            resultado(Aventurero, Mision, exitoso)).
 
-murio(Personaje):-
-    resultado(Personaje, _, fatal).
+murio(Aventurero):-
+    resultado(Aventurero, _, fatal).
 
-recompensaEnOro(Personaje, 0):-
-    murio(Personaje).
-recompensaEnOro(Personaje, RecompensaTotal):-
-    personaje(Personaje, _, _),
-    findall(Recompensa, (resultado(Personaje, Mision, exitoso), oroPorRealizar(Mision, Recompensa)), Recompensas),
+recompensaEnOro(Aventurero, 0):-
+    murio(Aventurero).
+recompensaEnOro(Aventurero, RecompensaTotal):-
+    aventurero(Aventurero, _, _),
+    not(murio(Aventurero)),
+    findall(Recompensa, (resultado(Aventurero, Mision, exitoso), oroPorRealizar(Mision, Recompensa)), Recompensas),
     sum_list(Recompensas, RecompensaTotal).
 
 oroPorRealizar(Mision, Oro):-
@@ -165,34 +166,34 @@ oroSegunCategoria(deBarrio, 2).
 oroSegunCategoria(aspirante, 15).
 oroSegunCategoria(heroica, 50).
 
-masRecompensado(Personaje):-
-    recompensaEnOro(Personaje, Recompensa),
+masRecompensado(Aventurero):-
+    recompensaEnOro(Aventurero, Recompensa),
     forall(
         (
-            recompensaEnOro(OtroPersonaje, RecompensaDelOtro),
-            OtroPersonaje \= Personaje
+            recompensaEnOro(OtroAventurero, RecompensaDelOtro),
+            OtroAventurero \= Aventurero
         ),
         Recompensa > RecompensaDelOtro
     ).
 
-:- begin_tests_con(parte2, [personaje(guybrush, pirata, 50)]).
+:- begin_tests_con(parte2, [aventurero(guybrush, pirata, 50)]).
 
-test("Cualquier personaje puede realizar la mision 'Ayudar a cruzar la calle'", set(Personaje == [rin, atalanta, kelsier, thorfinn, guybrush])):-
-    puedeRealizar(Personaje, ayudarACruzarLaCalle).
-test("Solo los personajes que sean fuertes pueden realizar la mision 'Ayudar a cortar lenia'", set(Personaje == [atalanta, thorfinn])):-
-    puedeRealizar(Personaje, ayudarACortarLenia).
-test("Solo los personajes que pueden disponer de un permiso real pueden realizar la mision 'escolta real de un diplomatico'", set(Personaje == [kelsier, guybrush])):-
-    puedeRealizar(Personaje, escoltaRealDe(diplomatico)).
-test("Solo los personajes que pueden disponer de un permiso real y sean nobles pueden realizar la mision 'escolta real de un diplomatico'", set(Personaje == [kelsier])):-
-    puedeRealizar(Personaje, escoltaRealDe(princesa)).
-test("Si un personaje es barbaro y agresivo puede realizar la mision 'pesadilla de la cueva'", nondet):-
+test("Cualquier aventurero puede realizar la mision 'Ayudar a cruzar la calle'", set(Aventurero == [rin, atalanta, kelsier, thorfinn, guybrush])):-
+    puedeRealizar(Aventurero, ayudarACruzarLaCalle).
+test("Solo los aventureros que sean fuertes pueden realizar la mision 'Ayudar a cortar lenia'", set(Aventurero == [atalanta, thorfinn])):-
+    puedeRealizar(Aventurero, ayudarACortarLenia).
+test("Solo los aventureros que pueden disponer de un permiso real pueden realizar la mision 'escolta real de un diplomatico'", set(Aventurero == [kelsier, guybrush])):-
+    puedeRealizar(Aventurero, escoltaRealDe(diplomatico)).
+test("Solo los aventureros que pueden disponer de un permiso real y sean nobles pueden realizar la mision 'escolta real de un diplomatico'", set(Aventurero == [kelsier])):-
+    puedeRealizar(Aventurero, escoltaRealDe(princesa)).
+test("Si un aventurero es barbaro y agresivo puede realizar la mision 'pesadilla de la cueva'", nondet):-
     puedeRealizar(thorfinn, pesadillaDeLaCueva).
-test("Si un personaje conoce un hechizo de fuego de potencia mayor o igual a 30, puede realizar la mision 'pesadilla de la cueva'"):-
+test("Si un aventurero conoce un hechizo de fuego de potencia mayor o igual a 30, puede realizar la mision 'pesadilla de la cueva'"):-
     puedeRealizar(atalanta, pesadillaDeLaCueva).
-test("Si un personaje no es un barbaro agresivo ni conoce un hechizo de fuego de potencia mayor o igual a 30, NO puede realizar la mision 'pesadilla de la cueva'"):-
+test("Si un aventurero no es un barbaro agresivo ni conoce un hechizo de fuego de potencia mayor o igual a 30, NO puede realizar la mision 'pesadilla de la cueva'"):-
     not(puedeRealizar(rin, pesadillaDeLaCueva)).
-test("Si un personaje tiene fondos para comprar pociones (20 oro) y sabe un hechizo de frio, puede realizar la mision 'rosas heladas'", set(Personaje == [rin])):-
-    puedeRealizar(Personaje, rosasHeladas(altasMontanias)).
+test("Si un aventurero tiene fondos para comprar pociones (20 oro) y sabe un hechizo de frio, puede realizar la mision 'rosas heladas'", set(Aventurero == [rin])):-
+    puedeRealizar(Aventurero, rosasHeladas(altasMontanias)).
 
 test("Una mision es facil si y solo si todos los aventureros pueden realizarla", set(Mision == [ayudarACruzarLaCalle])):-
     facil(Mision).
@@ -204,12 +205,12 @@ test("El resultado de una mision inentada es fatal si el aventurero NO podia rea
 test("El resultado de una mision intentada es fallido si el aventurero NO podia realizarla y la mision era de barrio o de aspirante", nondet):-
     resultado(kelsier, ayudarACortarLenia, fallido).
 
-test("Un aventurero es afortunado si cumplio exitosamente todas las misiones que intento", set(Personaje == [atalanta, thorfinn, guybrush])):-
-    afortunado(Personaje).
+test("Un aventurero es afortunado si cumplio exitosamente todas las misiones que intento", set(Aventurero == [atalanta, thorfinn, guybrush])):-
+    afortunado(Aventurero).
 
-test("Un personaje no obtiene nada de oro si murio (obtuvo un resultado fatal) en alguna mision", nondet):-
-    recompensaEnOro(rin, 0).
-test("El oro que obtiene un personaje que no murio es la suma del oro obtenido por cada una de sus misiones exitosas"):-
+test("Un aventurero no obtiene nada de oro si murio (obtuvo un resultado fatal) en alguna mision", set(Oro == [0])):-
+    recompensaEnOro(rin, Oro).
+test("El oro que obtiene un aventurero que no murio es la suma del oro obtenido por cada una de sus misiones exitosas"):-
     recompensaEnOro(thorfinn, 50).
 
 test("El oro que corresponde por una mision de barrio es 2"):-
@@ -219,7 +220,7 @@ test("El oro que corresponde por una mision de aspirante es 15"):-
 test("El oro que corresponde por una mision heroica es 50"):-
     oroPorRealizar(pesadillaDeLaCueva, 50).
 
-test("El personaje mas recompensado es aquel que mas oro debe cobrar como recompensa"):-
+test("El aventurero mas recompensado es aquel que mas oro debe cobrar como recompensa"):-
     masRecompensado(thorfinn).
 
 :- end_tests(parte2).
